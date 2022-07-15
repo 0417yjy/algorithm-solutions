7bd29f71b5984a6184c74e1adaa2346ac0099ce3
Jongyeon Yoon <0417yjy@naver.com>
2022-07-16 00:40:29 +0900

    Add 15649 (baekjoon)

---
+++ baekjoon/15649.java
@@ -0,0 +1,71 @@
+/*
+15649 N과 M (1)
+----------------------------------- 문제 -----------------------------------
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+----------------------------------- 입력 -----------------------------------
+첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+----------------------------------- 출력 -----------------------------------
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+----------------------------------- 예제 1 -----------------------------------
+3 1
+
+1
+2
+3
+----------------------------------- 예제 1 -----------------------------------
+4 2
+
+1 2
+1 3
+1 4
+2 1
+2 3
+2 4
+3 1
+3 2
+3 4
+4 1
+4 2
+4 3
+*/
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Main {
+    private static void print_permu(List<Integer> history_list, int tot_length, int max) {
+        int cur_length = history_list.size();        
+        if (cur_length == tot_length) {
+            for(Integer value : history_list) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= max; i++) {            
+            if (!history_list.contains(Integer.valueOf(i))) {
+                history_list.add(Integer.valueOf(i));
+                print_permu(history_list, tot_length, max);
+                history_list.remove(Integer.valueOf(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+            int N = s.nextInt();
+            int M = s.nextInt();
+            List<Integer> history_list = new ArrayList<Integer>(M);
+            
+            print_permu(history_list, M, N);
+
+            return;
+        }
+    }
+}
